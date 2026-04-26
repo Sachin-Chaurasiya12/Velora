52f9c519df46ad366145a91ebfb9ef1f4bcb0404
Sachin Chaurasiya <sachinchau444@gmail.com>
2026-04-26 15:59:53 +0530

    Program initialization

---
+++ Velora/src/test/java/com/Space/Velora/VeloraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Space.Velora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
